a44591a31b13d5ab3e0cca6494b4dcc4a2674bdd
RishabhHarman04 <er.23rishabh@gmail.com>
2022-03-28 15:24:13 +0530

    area

---
+++ main.py
@@ -0,0 +1,12 @@
+def area_rectangle(a,b):
+    result=a*b
+    print("area of Rectangle:", result)
+def perimeter_rectangle(a,b):
+    result=2*(l+b)
+    print("perimeter of Rectangle:", result)
+def area_square(a):
+    result=a*a
+    print("area of square:", result)
+if __name__ == "__main__":
+    num1 = float(input("enter first no."))
+    num2 = float(input("enter second no."))
